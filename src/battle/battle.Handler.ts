1a60e398772f687143c7163585d9b2e3501d7433
Monggle88 <fmonggle88@gmail.com>
2022-11-10 21:05:59 +0900

    파일 최신화를 위한 임시 저장

--- src/battle/battle.Handler.ts
+++ src/battle/battle.Handler.ts
@@ -7,11 +7,25 @@ export default {
         let tempScript: string = '';
 
         tempScript += '명령어 : \n';
-        tempScript += '수동 - 수동 전투를 진행합니다.\n';
-        tempScript += '자동 - 자동 전투를 진행합니다.\n';
-        tempScript += '돌아가기 - 이전 단계로 돌아갑니다.\n';
+        tempScript += '[수동] 전투 진행 - 수동 전투를 진행합니다.\n';
+        tempScript += '[자동] 전투 진행 - 자동 전투를 진행합니다.\n';
+        tempScript += '[돌]아가기 - 이전 단계로 돌아갑니다.\n';
 
         const script = tempScript;
+        const field = 'battle';
+        return { script, user, field };
+    },
+
+    fight: (CMD: string | undefined, user: UserSession) => {
+        let tempScript: string = '';
+
+        const script = tempScript;
+        const field = 'battle';
+        return { script, user, field };
+    },
+
+    auto: (CMD: string | undefined, user: UserSession) => {
+        const script = 'tempScript';
         const field = 'battle';
         return { script, user, field };
     },
@@ -24,7 +38,7 @@ export default {
         tempScript += `사용가능한 명령어가 궁금하시다면 '도움말'을 입력해보세요.\n`;
 
         const script = 'Error : \n' + tempScript;
-        const field = 'dungeon';
+        const field = 'battle';
         return { script, user, field };
     },
 };
