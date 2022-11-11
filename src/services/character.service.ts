2a2097d3bd9b79ff970407056cf17578d9babba5
ninthsun91 <ninthsun91@gmail.com>
2022-11-11 22:55:26 +0900

    change skill / equipment

--- src/services/character.service.ts
+++ src/services/character.service.ts
@@ -42,6 +42,26 @@ class CharacterService {
             name,
         });
     }
+
+    async changeSkill(characterId: number, ...skillId: number[]) {
+        if ( Array.isArray(skillId) && skillId.length > 3) {
+            throw new Error('스킬은 3개까지 등록할 수 있습니다.');
+        }
+        const skill = skillId.join(':');
+        await Characters.update({ skill }, {
+            where: { characterId }
+        });
+    }
+
+    async changeItem(characterId: number, ...itemId: number[]) {
+        if ( Array.isArray(itemId) && itemId.length > 2) {
+            throw new Error('장비는 2개까지 등록할 수 있습니다.');
+        }
+        const item = itemId.join(':');
+        await Characters.update({ item }, {
+            where: { characterId }
+        });
+    }
 }
 
 
