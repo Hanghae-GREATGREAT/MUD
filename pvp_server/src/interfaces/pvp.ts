bb6547015be27214c970fe19b3e9d052e71566d0
JangKroed <didlsdydgh@gmail.com>
2022-12-05 18:14:22 +0900

    #90 - 시련의장 userStatus 업데이트 및 버그 개선

--- pvp_server/src/interfaces/pvp.ts
+++ pvp_server/src/interfaces/pvp.ts
@@ -4,29 +4,18 @@ import { UserStatus } from "./user";
 interface PvpPlayer {
     socketId:string;
     userStatus?: UserStatus;
-    target?: string|undefined; // username
+    target?: string; // username
     selectSkill?: string; // skillname
 }
 
-interface Arranging {
-    multiples: number[];
-    attacks: number[];
-    target: string[];
-    userNames: string[];
-    roomName?: string;
-}
-
 interface pvpResult {
-    teamA: number[];
-    teamB: number[];
-    target: string[];
     userNames: string[];
+    target: string[];
     realDamage: number[];
     roomName?: string;
 }
 
 export {
     PvpPlayer,
-    Arranging,
     pvpResult,
 }
--- pvp_server/src/routes/pvpNpc.routes.ts
+++ pvp_server/src/routes/pvpNpc.routes.ts
@@ -7,8 +7,4 @@ router.post('/pvpTalk', pvpNpcController.pvpTalk);
 
 router.post('/pvpGo', pvpNpcController.pvpGo);
 
-// router.post('/wrongCommand', pvpController.wrongCommand);
-
-// router.post('/help', pvpController.help)
-
 export default router;
